d9711c6e6a8230cf457f053a8d4db7eeed61d7ca
minth1468 <tminab@connect.ust.hk>
2022-11-02 22:42:58 +0800

    Initial commit

---
+++ app/src/main/java/ece/course/pedometer/DisplayView.java
@@ -0,0 +1,125 @@
+package ece.course.pedometer;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.Surface;
+import android.view.SurfaceView;
+
+public class DisplayView extends SurfaceView {
+    public final static int TYPE_BALL    =0;
+    public final static int TYPE_SQUARE  =1;
+    public final static int TYPE_DIAMOND =2;
+    public final static int TYPE_ARC     =3;
+
+    private float mCenterX =0.0f;
+    private float mCenterY =0.0f;
+    private float mRadius =0.0f;
+    private float mRectHalfWidth =0.0f;
+    private float mRectHalfHeight =0.0f;
+    private float mRectStep =0.0f;
+
+    private float mPtrCenterX  =100.0f;
+    private float mPtrCenterY  =100.0f;
+    private float mPtrRadius   =10.0f;
+
+    private int mPtrType = TYPE_BALL;
+    private int mPtrColor = Color.GRAY;
+
+    public void setPtr(float posX, float posY) {
+        mPtrCenterX = posX * mRadius * 0.9f + mCenterX;
+        mPtrCenterY = (posY * mRadius * 0.9f) + mCenterY;
+        invalidate();
+    }
+    public void setPtrColor(int color) {
+        mPtrColor = color;
+        invalidate();
+    }
+    public void setPtrType(int type) {
+        mPtrType = type;
+        invalidate();
+    }
+
+    public DisplayView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setWillNotDraw(false);
+    }
+    public void onDraw(Canvas canvas){
+        if(canvas == null)
+            return;
+
+        canvas.drawColor(Color.WHITE);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        // canvas.drawCircle(40.0f,40.0f,10.0f,paint);
+        canvas.drawRect(mCenterX-mRectHalfWidth, mCenterY-mRectHalfHeight, mCenterX+mRectHalfWidth, mCenterY+mRectHalfHeight, paint);
+        paint.setColor(Color.YELLOW);
+        canvas.drawRect(mCenterX-mRectHalfWidth, mCenterY-mRectHalfHeight+mRectStep, mCenterX+mRectHalfWidth, mCenterY+mRectHalfHeight-mRectStep, paint);
+        paint.setColor(Color.BLUE);
+        canvas.drawRect(mCenterX-mRectHalfWidth, mCenterY-mRectHalfHeight+2*mRectStep, mCenterX+mRectHalfWidth, mCenterY+mRectHalfHeight-2*mRectStep, paint);
+        paint.setColor(Color.BLACK);
+        canvas.drawRect(mCenterX-3*mRectHalfWidth, mCenterY-0.01f*mRectHalfHeight, mCenterX+3*mRectHalfWidth, mCenterY+0.01f*mRectHalfHeight, paint);
+
+        paint.setColor(mPtrColor);
+        switch(mPtrType) {
+            case TYPE_BALL:
+                canvas.drawCircle(mPtrCenterX, mPtrCenterY, mPtrRadius, paint);
+                break;
+            case TYPE_SQUARE :
+                canvas.drawRect(mPtrCenterX - mPtrRadius, mPtrCenterY - mPtrRadius,
+                        mPtrCenterX + mPtrRadius, mPtrCenterY + mPtrRadius, paint);
+                break;
+            case TYPE_DIAMOND :
+                Path path = new Path();
+                path.moveTo(mPtrCenterX, mPtrCenterY - mPtrRadius);
+                path.lineTo(mPtrCenterX - mPtrRadius, mPtrCenterY);
+                path.lineTo(mPtrCenterX, mPtrCenterY + mPtrRadius);
+                path.lineTo(mPtrCenterX + mPtrRadius, mPtrCenterY);
+                path.close();
+                canvas.drawPath(path, paint);
+                break;
+            case TYPE_ARC :
+                canvas.drawArc(new RectF(mPtrCenterX - mPtrRadius, mPtrCenterY - mPtrRadius,
+                        mPtrCenterX + mPtrRadius, mPtrCenterY + mPtrRadius), -45.0f, -90.0f, true, paint);
+                break;
+        }
+
+        /*paint.setColor(Color.BLUE);
+        canvas.drawRect(70.0f,30.0f,90.0f,50.0f,paint);
+
+        paint.setColor(Color.GREEN);
+        Path path = new Path();
+        path.moveTo(40.0f,70.0f);
+        path.lineTo(30.0f,80.0f);
+        path.lineTo(40.0f,90.0f);
+        path.lineTo(50.0f,80.0f);
+        path.close();
+
+        canvas.drawPath(path,paint);
+
+        paint.setColor(Color.WHITE);
+        canvas.drawArc(new RectF(70.0f,70.0f,90.0f,90.0f),
+                -45.0f,-90.0f,true,paint);    */
+
+    }
+
+    public void onSizeChanged(int width, int height, int oldWidth, int oldHeight){
+        mCenterX = width/2;
+        mCenterY = height/2;
+        mRadius = ((width < height)? width : height)*6.0f /8.0f;
+        mRectHalfWidth = ((width < height)? width : height)*1.0f /12.0f;
+        mRectHalfHeight = ((width > height)? width : height)*3.5f /8.0f;
+        mRectStep = mRectHalfHeight/3;
+
+        mPtrCenterX = mCenterX;
+        mPtrCenterY = mCenterY;
+        mPtrRadius = mRadius / 10.0f;
+        invalidate();
+    }
+}
---
+++ app/src/main/java/ece/course/pedometer/MainActivity.java
@@ -0,0 +1,155 @@
+package ece.course.pedometer;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.PowerManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private final static float MAX_GRAVITY = 9.82f;
+    private final static float MAX_FORCE = (float) (Math.sqrt(MAX_GRAVITY*MAX_GRAVITY*2));
+    private final static float DETECT_THRESHOLD = 0.1f;
+    private float mX = -100.0f;
+    private float mY = -100.0f;
+    private float mZ = -100.0f;
+    private float mF = 0.0f;
+    private int mWalkCnt = 0;
+    private boolean mWalkCnted = false;
+    private int mJmpCnt = 0;
+    private int mJmpTimeCnt = 0;
+    private boolean mJmpCnted = false;
+    private final static int JMP_TIME_THRESHOLD = 5;
+
+    private DisplayView mDisplayView;
+    private PedometerSensor mPedometerSensor;
+
+    private PowerManager mPowerManager;
+    private PowerManager.WakeLock mWakeLock;
+
+
+    @SuppressLint("HandlerLeak")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        mPowerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                getClass().getName());
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDisplayView = (DisplayView)findViewById(R.id.mDisplayView);
+        mPedometerSensor = new PedometerSensor(this, new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                float tmpX = msg.getData().getFloat(PedometerSensor.TAG_VALUE_DX);
+                float tmpY = -msg.getData().getFloat(PedometerSensor.TAG_VALUE_DY);
+                float tmpZ = msg.getData().getFloat(PedometerSensor.TAG_VALUE_DZ);
+                if (tmpX - mX > DETECT_THRESHOLD || tmpX - mX < -DETECT_THRESHOLD ||
+                        tmpY - mY > DETECT_THRESHOLD || tmpY - mY < -DETECT_THRESHOLD ||
+                        tmpZ - mZ > DETECT_THRESHOLD || tmpZ - mZ < -DETECT_THRESHOLD) {
+                    mX = tmpX;
+                    mY = tmpY;
+                    mZ = tmpZ;
+                    mF = (float) Math.sqrt(mX*mX+mY*mY+mZ*mZ);
+                    TextView tvValueX = (TextView) findViewById(R.id.tvValueX);
+                    TextView tvValueY = (TextView) findViewById(R.id.tvValueY);
+                    TextView tvValueZ = (TextView) findViewById(R.id.tvValueZ);
+                    TextView tvValueF = (TextView) findViewById(R.id.tvValueF);
+                    TextView tvValueWC = (TextView) findViewById(R.id.tvValueWC);
+                    TextView tvValueJC = (TextView) findViewById(R.id.tvValueJC);
+                    tvValueX.setText("" + mX);
+                    tvValueY.setText("" + mY);
+                    tvValueZ.setText("" + mZ);
+                    tvValueF.setText("" + mF);
+                    tvValueWC.setText("" + mWalkCnt);
+                    tvValueJC.setText("" + mJmpCnt);
+                    mDisplayView.setPtr(0, (float) ((mF - 9.81)/ MAX_FORCE));
+
+                    // Walk Count, Verified to be accurate
+                    if ((mF - 9.81) > 6 && mWalkCnted == false) {
+                        mWalkCnted = true;
+                        mWalkCnt = mWalkCnt + 1;
+                    }
+                    if ((mF - 9.81) <= 6 && mWalkCnted == true) {
+                        mWalkCnted = false;
+                    }
+
+                    // Jump Count, Verified to be accurate
+                    if (mF < 2 && mJmpCnted == false) {
+                        mJmpTimeCnt = 0;
+                        mJmpCnted = true;
+                        mJmpCnt = mJmpCnt + 1;
+                        mWalkCnt = mWalkCnt - 2;
+                        if (mWalkCnt < 0) {
+                            mWalkCnt = 0;
+                        }
+                    }
+                    if (mF >= 2 && mJmpCnted == true) {
+                        mJmpTimeCnt = mJmpTimeCnt + 1;
+                        if (mJmpTimeCnt > JMP_TIME_THRESHOLD) {
+                            mJmpCnted = false;
+                        }
+                    }
+
+                }
+            }
+        });
+    }
+
+    public synchronized void onResume() {
+        super.onResume();
+        mWakeLock.acquire();
+        if (mPedometerSensor != null) {
+            mPedometerSensor.startListening();
+        }
+    }
+    public synchronized void onPause() {
+        mWakeLock.release();
+        if (mPedometerSensor != null) {
+            mPedometerSensor.stopListening();
+        }
+        super.onPause();
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuPtrBall :
+                mDisplayView.setPtrType(DisplayView.TYPE_BALL);
+                return true;
+            case R.id.menuPtrSquare :
+                mDisplayView.setPtrType(DisplayView.TYPE_SQUARE);
+                return true;
+            case R.id.menuPtrDiamond :
+                mDisplayView.setPtrType(DisplayView.TYPE_DIAMOND);
+                return true;
+            case R.id.menuPtrArc :
+                mDisplayView.setPtrType(DisplayView.TYPE_ARC);
+                return true;
+            case R.id.menuPtrRed :
+                mDisplayView.setPtrColor(Color.RED);
+                return true;
+            case R.id.menuPtrBlue :
+                mDisplayView.setPtrColor(Color.BLUE);
+                return true;
+            case R.id.menuPtrGreen :
+                mDisplayView.setPtrColor(Color.GREEN);
+                return true;
+            case R.id.menuPtrWhite :
+                mDisplayView.setPtrColor(Color.WHITE);
+                return true;
+        }
+        return false;
+    }
+
+}
